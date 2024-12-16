d64ac446a94366e396a8ba69b297aa34bbfaf263
snehjp2 <snehjp2@gmail.com>
2024-12-16 14:11:24 -0600

    updating test

--- src/scripts/toy_test.py
+++ src/scripts/toy_test.py
@@ -12,15 +12,13 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch.nn as nn
 # from toy_models import d4_model
-from toy_model_simple import mnistm_models
-from toy_dataset import MnistM
+from toy_model_simple import evo_models
+from toy_dataset import GZEvo
 from utils import OnePixelAttack
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-classes = (
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-)
+classes  = ('barred_spiral', 'edge_on_disk', 'featured_without_bar_or_spiral', 'smooth_cigar', 'smooth_round', 'unbarred_spiral')
 
 
 def load_models(directory_path, model_name):
@@ -33,7 +31,7 @@ def load_models(directory_path, model_name):
         if file_name.endswith('.pt'):
             file_path = os.path.join(directory_path, file_name)
             print(f'Loading {model_name} from {file_path}...')
-            model = mnistm_models[model_name](num_classes=10)
+            model = evo_models[model_name](num_classes=10)
             # model = d4_model() if model_name == 'D4' else cnn()
             model.eval()
             model.load_state_dict(torch.load(file_path, map_location=device))
@@ -117,7 +115,7 @@ def main(model_dir, output_name, x_test_path, y_test_path, model_name, N=None, a
             transforms.Resize(32)
         ])
 
-    test_dataset = MnistM(x_test_path, y_test_path, transform=transform)
+    test_dataset = GZEvo(x_test_path, y_test_path, transform=transform)
     test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=True)
 
     models = load_models(model_dir, model_name)
