2f428379a43cbb0ce07efb56cb35181ec3611f97
snehjp2 <snehjp2@gmail.com>
2025-01-20 14:55:39 -0600

    bug

--- src/scripts/train_SIDDA.py
+++ src/scripts/train_SIDDA.py
@@ -576,7 +576,8 @@ def train_SIDDA(
 
 def main(config):
     model_name = str(config["model"])
-    model = model_dict[str(config["dataset"])][model_name]()
+    dataset_name = str(config["dataset"])
+    model = model_dict[dataset_name][model_name]()
 
     params_to_optimize = [p for p in model.parameters() if p.requires_grad]
     optimizer = optim.AdamW(
@@ -592,8 +593,8 @@ def main(config):
     )
 
     if (
-        dataset_dict[str(config["dataset"])] == "shapes"
-        or dataset_dict[config["dataset"]] == "astro_objects"
+        dataset_dict[dataset_name] == "shapes"
+        or dataset_dict[dataset_name] == "astro_objects"
     ):
         train_transform = transforms.Compose(
             [
@@ -614,7 +615,7 @@ def main(config):
                 transforms.Resize(100),
             ]
         )
-    elif dataset_dict[str(config["dataset"])] == "mnist_m":
+    elif dataset_dict[dataset_name] == "mnist_m":
         train_transform = transforms.Compose(
             [
                 transforms.ToTensor(),
@@ -635,7 +636,7 @@ def main(config):
             ]
         )
 
-    elif dataset_dict[str(config["dataset"])] == "gz_evo":
+    elif dataset_dict[dataset_name] == "gz_evo":
         train_transform = transforms.Compose(
             [
                 transforms.ToTensor(),
@@ -669,7 +670,7 @@ def main(config):
 
     print("Loading datasets!")
     start = time.time()
-    train_dataset = dataset_dict[str(config["dataset"])](
+    train_dataset = dataset_dict[dataset_name](
         input_path=config["train_data"]["input_path"],
         output_path=config["train_data"]["output_path"],
         transform=train_transform,
@@ -682,7 +683,7 @@ def main(config):
         val_transform=val_transform,
     )
 
-    target_dataset = dataset_dict[str(config["dataset"])](
+    target_dataset = dataset_dict[dataset_name](
         input_path=config["train_data"]["target_input_path"],
         transform=train_transform,
     )
